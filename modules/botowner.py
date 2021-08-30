fc9efa0a09ec250c539b435f9cfc8d9cddf61dc8
windowsboy111 <wboy111@outlook.com>
2021-08-30 23:31:52 +0800

    fixed exec command

--- modules/botowner.py
+++ modules/botowner.py
@@ -37,7 +37,7 @@ async def _exec(ctx, *, code='await ctx.send("????")'):
         exec(realcode, globals(), locals())
         for task in asyncio.all_tasks(ctx.bot.loop):
             if task.get_name() == '_exec':
-                asyncio.wait_for(task)
+                await asyncio.wait_for(task, timeout=0)
                 res = task.result()
                 if res:
                     await ctx.send(f"returned: {res}")
