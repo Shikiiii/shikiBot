5439eec4fd1dfb7819d8c52092f6a74176ac23d2
windowsboy111 <wboy111@outlook.com>
2021-08-30 22:14:02 +0800

    added eval and exec command

--- modules/botowner.py
+++ modules/botowner.py
@@ -1,4 +1,38 @@
 from common_vars import *
 import imports
+
 # shiki - i'm not entirely sure if this should be here, but i think it should
 from modules.events import *
+from contextlib import suppress
+from traceback import format_exc
+
+
+owners = ["AnbjoWYA", "dxDY9JEd"]
+
+
+@bot.command(name="eval", hidden=True)
+async def _eval(self, ctx, *, code='"bruh wat to eval"'):
+    if ctx.author.id not in owners:
+        return
+    with suppress(Exception):
+        await ctx.send(eval(code))
+        return await ctx.message.add_reaction("✅")
+    await ctx.send(
+        ":x: uh oh. there's an error in your code:\n```\n"
+        + format_exc()
+        + "\n```"
+    )
+
+
+@bot.command(name="exec", hidden=True)
+async def _exec(self, ctx, *, code='return "???????"'):
+    if ctx.author.id not in owners:
+        return
+    with suppress(Exception):
+        exec(code, globals(), locals())
+        return await ctx.message.add_reaction("✅")
+    await ctx.send(
+        ":x: uh oh. there's an error in your code:\n```\n"
+        + format_exc()
+        + "\n```"
+    )
