4b7da2d1a4bbc42884b3810b299997988aae7508
shiki <33571437+Shikiiii@users.noreply.github.com>
2021-08-30 16:26:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from common_vars import *
 from imports import *
 
 import modules.general
+import modules.economy
+import modules.events
+import modules.eh
 
 bot.run(os.environ.get("EMAIL"), os.environ.get("PASSWORD"))
