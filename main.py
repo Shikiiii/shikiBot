a47420c348560be214ad8e8bf9764a0610c86806
windowsboy111 <wboy111@outlook.com>
2021-08-31 23:11:11 +0800

    tidy up

--- main.py
+++ main.py
@@ -2,28 +2,33 @@ from common_vars import *
 from imports import *
 from typing import Union, Any
 
+
 @bot.event
 async def on_command_error(ctx, error):
-	print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-	traceback.print_exception(type(error), error, None, file=sys.stderr)
+    print("Ignoring exception in command {}:".format(ctx.command), file=sys.stderr)
+    traceback.print_exception(type(error), error, None, file=sys.stderr)
+
 
 @bot.command()
 async def myid(ctx):
-	await ctx.send(ctx.message.author.id)
+    await ctx.send(ctx.message.author.id)
+
 
 # temporary command
-@bot.command(name='id')
+@bot.command(name="id")
 async def _id(ctx, thing: Union[guilded.ChatChannel, guilded.User, Any]):
     try:
         await ctx.send(thing.id)
     except:
         pass
 
+
 @bot.command()
 async def test(ctx, *, s):
-	await ctx.send(s)
+    await ctx.send(s)
 
-bot.load_extension('modules.general')
-bot.load_extension('modules.economy')
-bot.load_extension('modules.botowner')
+
+bot.load_extension("modules.general")
+bot.load_extension("modules.economy")
+bot.load_extension("modules.botowner")
 bot.run(os.environ.get("EMAIL"), os.environ.get("PASSWORD"))
