98cb1ca126ed311b78380cda94d6f1df2af96ee0
shiki <33571437+Shikiiii@users.noreply.github.com>
2021-08-31 13:30:48 +0300

    Update economy.py

--- modules/economy.py
+++ modules/economy.py
@@ -1,55 +1,57 @@
 from common_vars import *
 import imports
-# shiki - i'm not entirely sure if this should be here, but i think it should
-#from modules.events import *
 
-@bot.command()
-async def balance(ctx, user):
-	conv = commands.converters.MemberConverter()
-	try:
-		member = await conv.convert(ctx, user)
-	except:
-		await ctx.send(f"<@{ctx.message.author.id}>, member not found. :(\nTry out our `s!help` command if you're stuck.")
-		return
-	if member.id in money:
-		await ctx.send(f"<@{member.id}>'s balance is {money.get(member.id)}$.")
-	else:
-		await register_in_money_db(member.id)
-		await ctx.send(f"<@{member.id}>'s balance is 100$.")
+class Economy(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
 
-@bot.command()
-async def daily(ctx, user):
-	conv = commands.converters.MemberConverter()
-	try:
-		member = await conv.convert(ctx, user)
-	except:
-		await ctx.send(f"<@{ctx.message.author.id}>, member not found. :(\nTry out our `s!help` command if you're stuck.")
-		return
-	if member.id in money:
-		# ...
-		if member.id not in claimed_daily:
-			money[member.id] += 1000
-			push_money()
-			if ctx.message.author.id == member.id:
-				await ctx.send(f"<@{ctx.message.author.id}>, you've claimed your daily bonus of 1000$.")
-				claimed_daily.append(member.id)
-			else:
-				await ctx.send(f"<@{ctx.message.author.id}>, you've given your daily bonus of 1000$ to {member.name}. How nice!")
-				claimed_daily.append(ctx.message.author.id)
-			push_daily()
+	@commands.command()
+	async def balance(self, ctx, user):
+		conv = commands.converters.MemberConverter()
+		try:
+			member = await conv.convert(ctx, user)
+		except:
+			await ctx.send(f"<@{ctx.message.author.id}>, member not found. :(\nTry out our `s!help` command if you're stuck.")
+			return
+		if member.id in money:
+			await ctx.send(f"<@{member.id}>'s balance is {money.get(member.id)}$.")
 		else:
-			await ctx.send(f"<@{ctx.message.author.id}>, it looks like you've already claimed your daily. Try again later!")
-	else:
-		await register_in_money_db(member.id)
-		if member.id not in claimed_daily:
-			money[member.id] += 1000
-			push_money()
-			if ctx.message.author.id == member.id:
-				await ctx.send(f"<@{ctx.message.author.id}>, you've claimed your daily bonus of 1000$.")
-				claimed_daily.append(member.id)
+			await register_in_money_db(member.id)
+			await ctx.send(f"<@{member.id}>'s balance is 100$.")
+
+	@commands.command()
+	async def daily(self, ctx, user):
+		conv = commands.converters.MemberConverter()
+		try:
+			member = await conv.convert(ctx, user)
+		except:
+			await ctx.send(f"<@{ctx.message.author.id}>, member not found. :(\nTry out our `s!help` command if you're stuck.")
+			return
+		if member.id in money:
+			# ...
+			if member.id not in claimed_daily:
+				money[member.id] += 1000
+				push_money()
+				if ctx.message.author.id == member.id:
+					await ctx.send(f"<@{ctx.message.author.id}>, you've claimed your daily bonus of 1000$.")
+					claimed_daily.append(member.id)
+				else:
+					await ctx.send(f"<@{ctx.message.author.id}>, you've given your daily bonus of 1000$ to {member.name}. How nice!")
+					claimed_daily.append(ctx.message.author.id)
+				push_daily()
 			else:
-				await ctx.send(f"<@{ctx.message.author.id}>, you've given your daily bonus of 1000$ to {member.name}. How nice!")
-				claimed_daily.append(ctx.message.author.id)
-			push_daily()
+				await ctx.send(f"<@{ctx.message.author.id}>, it looks like you've already claimed your daily. Try again later!")
 		else:
-			await ctx.send(f"<@{ctx.message.author.id}>, it looks like you've already claimed your daily. Try again later!")
+			await register_in_money_db(member.id)
+			if member.id not in claimed_daily:
+				money[member.id] += 1000
+				push_money()
+				if ctx.message.author.id == member.id:
+					await ctx.send(f"<@{ctx.message.author.id}>, you've claimed your daily bonus of 1000$.")
+					claimed_daily.append(member.id)
+				else:
+					await ctx.send(f"<@{ctx.message.author.id}>, you've given your daily bonus of 1000$ to {member.name}. How nice!")
+					claimed_daily.append(ctx.message.author.id)
+				push_daily()
+			else:
+				await ctx.send(f"<@{ctx.message.author.id}>, it looks like you've already claimed your daily. Try again later!")
