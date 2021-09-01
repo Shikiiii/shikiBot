8f088fb7626ce8c1c0006092e857feb355adb77d
shiki <33571437+Shikiiii@users.noreply.github.com>
2021-09-01 12:13:15 +0300

    reverting previous change

--- modules/events.py
+++ modules/events.py
@@ -125,7 +125,7 @@ class Events(commands.Cog):
                             "That command/module doesn't exist yet. Check out all the cool commands with `s!help`. :sunglasses:"
                         )
             return
-        #await self.bot.process_commands(msg)
+        await self.bot.process_commands(msg)
 
     @commands.Cog.listener()
     async def on_message_delete(self, msg):
